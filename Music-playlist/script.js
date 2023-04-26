914e95d0278ba9c2725a76141ae2b46ecef4ca03
Fraol Bekele <fraolbekele4@gmail.com>
2023-04-26 14:55:01 -0500

    Update format of time on progress bar

--- Music-playlist/script.js
+++ Music-playlist/script.js
@@ -10,11 +10,17 @@ const prevButton = document.querySelector('#prev');
 const playPauseButton = document.querySelector('.play-pause');
 const nextButton = document.querySelector('#next');
 const shuffleButton = document.querySelector('#shuffle');
-
 const playPauseIcon = document.querySelector(".play-pause i");
- 
+const progBar = document.querySelector('.progess-bar');
+const progArea = document.querySelector('.progress-area');
+const elapsed = document.querySelector('.current-time')
+const remainingTime = document.querySelector('.remaining-time')
 
-let index = 7;
+
+
+
+
+let index = Math.floor(Math.random()*songs.length);
 
 window.addEventListener("load", ()=>{
     loadMusic(index);
@@ -81,17 +87,33 @@ function playPrev(){
     playSong();
 }
 
-// repeat song event listner
-repeatButton.addEventListener("click", ()=>{
-    repeatSong();
-})
+// event listener to an Audio element 
+//that updates a progress bar as the audio plays
+Audio.addEventListener("timeupdate", (e)=>{
+    const startTime = e.target.currentTime;
+    const finalTime = e.target.duration;
+    let barWidth = (startTime/finalTime)*100;
+    progBar.style.width = barWidth + "%";
 
 
-// repeat song function
-function repeatSong(){
+    //Format elapsed time
+    const elapsedMinutes = Math.floor(startTime/60);
+    const elapsedSeconds = Math.floor(startTime % 60);
+    const elapsedFormated = `${elapsedMinutes}:${elapsedSeconds.toString().padStart(2, '0')}`;
+    elapsed.textContent = elapsedFormated;
 
-}
+    // Formate time remaining
+    const remainingSeconds = Math.floor(finalTime - startTime);
+    const remainingFormatted = `-${Math.floor(remainingSeconds / 60)}:${(remainingSeconds % 60).toString().padStart(2, '0')}`;
+    remainingTime.textContent = remainingFormatted;
 
+    progArea.addEventListener("click", (e) =>{
+        let progressValue = progArea.clientWidth; //width of progress bar
+        let clickedOffsetX = e.offsetX; // X position of the click
+        let songDuration = Audio.duration; //total duation of the audio
+        Audio.currentTime = (clickedOffsetX / progressValue)*songDuration;
+    })
+});
 
 
 
